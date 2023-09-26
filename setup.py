e4e4481fdf2e607c1a0118661279306ffc101580
Pratik94229 <pratik.941@gmail.com>
2023-09-26 18:20:37 +0530

    requiremensts added and env created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Skywalker',
+    author_email= 'pratik.941@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
